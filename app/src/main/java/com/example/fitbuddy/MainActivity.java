bc91ff50d6d0677025867dd53b47f9a04950df31
Ryotiu <Rhs0988762>
2022-01-26 22:23:14 -0800

    Commit

--- app/src/main/java/com/example/fitbuddy/MainActivity.java
+++ app/src/main/java/com/example/fitbuddy/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.fitbuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button LoginBtn, RegisterBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LoginBtn = (Button) findViewById(R.id.LoginBtn);
+        RegisterBtn = (Button) findViewById(R.id.RegisterButton);
+
+        LoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLogin();
+            }
+        });
+
+        RegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openRegister();
+            }
+        });
     }
+
+    public void openLogin() {
+        Intent loginIntent = new Intent(this, Login.class);
+        startActivity(loginIntent);
+    }
+
+    public void openRegister() {
+        Intent registerIntent = new Intent(this, Register.class);
+        startActivity(registerIntent);
+    }
+
 }

8e090920bf6243e81e6e5eb9aeb2ff9d40421ea2
Ryotiu <Rhs0988762>
2022-01-26 22:29:17 -0800

    Commit

--- app/src/main/java/com/example/fitbuddy/MainActivity.java
+++ app/src/main/java/com/example/fitbuddy/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    //Test
     public void openLogin() {
         Intent loginIntent = new Intent(this, Login.class);
         startActivity(loginIntent);

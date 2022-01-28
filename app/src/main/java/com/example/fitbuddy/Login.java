425d18e1e75a5fbd3b277c27effb4d3d1b1d376f
Ryotiu <Rhs0988762>
2022-01-27 20:41:56 -0800

    Commit

--- app/src/main/java/com/example/fitbuddy/Login.java
+++ app/src/main/java/com/example/fitbuddy/Login.java
@@ -3,12 +3,21 @@ package com.example.fitbuddy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends AppCompatActivity {
+
+    private EditText loginEmail, loginPasswd;
+    private Button registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginEmail = (EditText) findViewById(R.id.LoginEmail);
+        loginPasswd = (EditText) findViewById(R.id.LoginPasswd);
+        registerBtn = (Button) findViewById(R.id.LoginBtn);
     }
 }
--- app/src/main/java/com/example/fitbuddy/Register.java
+++ app/src/main/java/com/example/fitbuddy/Register.java
@@ -3,12 +3,27 @@ package com.example.fitbuddy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Register extends AppCompatActivity {
+
+    private EditText firstName, lastName, registerEmail, registerPassWD, birthday, currentWeight, targetwWeight;
+    private Button registerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        firstName = (EditText) findViewById(R.id.FirstName);
+        lastName = (EditText) findViewById(R.id.LastName);
+        registerEmail = (EditText) findViewById(R.id.RegisterEmail);
+        registerPassWD = (EditText) findViewById(R.id.RegisterPasswd);
+        birthday = (EditText) findViewById(R.id.Birthday);
+        currentWeight = (EditText) findViewById(R.id.CurrentWeight);
+        targetwWeight = (EditText) findViewById(R.id.TargetWeight);
+
+        registerBtn = (Button) findViewById(R.id.RegisterBtn);
     }
 }

d6d80487548d854c41571110eda51bb5fa087e05
Aspen <sp-clariti@avocette.com.staging>
2022-02-01 23:51:54 -0800

    Add WeeklyTracker class Add MonthlyTracker class Modified Trackers class Modified trackers activity Modified weekly tracker activity Modified monthly tracker activity Modified AndroidManifest

--- app/src/main/java/com/example/fitbuddy/Trackers.java
+++ app/src/main/java/com/example/fitbuddy/Trackers.java
@@ -1,14 +1,42 @@
 package com.example.fitbuddy;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Trackers extends AppCompatActivity {
+
+    private Button WeeklyTrackerBtn;
+    private Button MonthlyTrackerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trackers);
+
+        WeeklyTrackerBtn = findViewById(R.id.WeeklyTrackerBtn);
+        MonthlyTrackerBtn = findViewById(R.id.MonthlyTrackerBtn);
+
+        // Launch activity_weekly_tracker
+        WeeklyTrackerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Trackers.this, WeeklyTracker.class));
+                finish();
+            }
+        });
+
+        // Launch activity_monthly_tracker
+        MonthlyTrackerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Trackers.this, MonthlyTracker.class));
+                finish();
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/example/fitbuddy/WeeklyTracker.java
@@ -0,0 +1,14 @@
+package com.example.fitbuddy;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WeeklyTracker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weekly_tracker);
+
+    }
+}

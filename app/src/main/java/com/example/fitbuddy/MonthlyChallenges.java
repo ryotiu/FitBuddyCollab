4b6c0ab9476983e769004f2913acd0a8ef02a984
aashinikaushik <aashinikaushik@gmail.com>
2022-02-05 15:38:57 +0530

    Add Challenges Activites

---
+++ app/src/main/java/com/example/fitbuddy/MonthlyChallenges.java
@@ -0,0 +1,15 @@
+package com.example.fitbuddy;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MonthlyChallenges extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monthly_challenges);
+
+    }
+}
---
+++ app/src/main/java/com/example/fitbuddy/WeeklyChallenges.java
@@ -0,0 +1,15 @@
+package com.example.fitbuddy;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WeeklyChallenges extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weekly_challenges);
+
+    }
+}

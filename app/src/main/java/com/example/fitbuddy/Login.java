a4a581a99cf4668beb2a89c94a213d03f7d7d19a
Ryotiu <Rhs0988762>
2022-02-06 23:56:24 -0800

    Commit Improved Registration Error Handling

--- app/src/main/java/com/example/fitbuddy/Login.java
+++ app/src/main/java/com/example/fitbuddy/Login.java
@@ -39,8 +39,10 @@ public class Login extends AppCompatActivity {
 
         loginBtn = (Button) findViewById(R.id.LoginBtn);
 
+        //Get Firebase Auth Instance
         firebaseAuth = FirebaseAuth.getInstance();
 
+        //No Account Click Listener
         noAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -48,6 +50,15 @@ public class Login extends AppCompatActivity {
             }
         });
 
+        //Forget Password Click Listener
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Add Code here
+            }
+        });
+
+        //Login Button Click Listener
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -56,16 +67,21 @@ public class Login extends AppCompatActivity {
         });
     }
 
+    //Login the user
     private void loginUser(){
         String lEmail = loginEmail.getText().toString();
         String lPassword = loginPassword.getText().toString();
 
+        //If Email and Password are not empty
+        //Email is a correct Email Address
+        //Login the user
         if(!lEmail.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(lEmail).matches()){
             if(!lPassword.isEmpty()){
                 firebaseAuth.signInWithEmailAndPassword(lEmail, lPassword)
                         .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
                             @Override
                             public void onSuccess(AuthResult authResult) {
+                                //Add SharedPreference for all Signed In Activity from here on
                                 Toast.makeText(Login.this, "Successfully Login", Toast.LENGTH_SHORT).show();
                                 startActivity(new Intent(Login.this, LoggedIn.class));
                             }
--- app/src/main/java/com/example/fitbuddy/Register.java
+++ app/src/main/java/com/example/fitbuddy/Register.java
@@ -15,15 +15,17 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Register extends AppCompatActivity {
 
-    private EditText firstName, lastName, registerEmail, registerPassWD, birthday, currentWeight, targetwWeight;
+    private EditText firstName, lastName, registerEmail, registerPassWD, birthday, currentWeight, targetWeight;
     private Button registerBtn;
     private TextView haveAccount;
 
@@ -42,13 +44,14 @@ public class Register extends AppCompatActivity {
         registerPassWD = (EditText) findViewById(R.id.RegisterPasswd);
         birthday = (EditText) findViewById(R.id.Birthday);
         currentWeight = (EditText) findViewById(R.id.CurrentWeight);
-        targetwWeight = (EditText) findViewById(R.id.TargetWeight);
+        targetWeight = (EditText) findViewById(R.id.TargetWeight);
         haveAccount = (TextView) findViewById(R.id.HaveAccount);
-
         registerBtn = (Button) findViewById(R.id.RegisterBtn);
 
+        //Get Firebase Auth Instance
         firebaseAuth = FirebaseAuth.getInstance();
 
+        //Registered user Click Listener
         haveAccount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -56,6 +59,7 @@ public class Register extends AppCompatActivity {
             }
         });
 
+        //Register Button Click Listener
         registerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -64,50 +68,96 @@ public class Register extends AppCompatActivity {
         });
     }
 
+    //Create a User
     private void createUser(){
 
+        //Get Firebase Database Root Instance
         root = FirebaseDatabase.getInstance();
+
+        //Get Users Reference path
         ref = root.getReference("Users");
 
+        //Then save all the input values as string
         String fName = firstName.getText().toString();
         String lName = lastName.getText().toString();
         String rEmail = registerEmail.getText().toString();
         String rPassword = registerPassWD.getText().toString();
         String rBirthday = birthday.getText().toString();
         String rCWeight = currentWeight.getText().toString();
-        String rTWeight = targetwWeight.getText().toString();
+        String rTWeight = targetWeight.getText().toString();
 
-        UserClass User = new UserClass(fName, lName, rEmail, rPassword, rBirthday, rCWeight, rTWeight);
+        //Check if first name, last name, birthday, current weight, target weight field is empty
+        if(!fName.isEmpty() && !lName.isEmpty() && !rBirthday.isEmpty() && !rCWeight.isEmpty() && !rTWeight.isEmpty()) {
 
-        if(!rEmail.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(rEmail).matches()){
-            if(!rPassword.isEmpty()){
-                firebaseAuth.createUserWithEmailAndPassword(rEmail, rPassword)
-                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                String[] splitEmail = rEmail.split("@");
-                                String username = splitEmail[0];
-                                ref.child(username).setValue(User);
-                                Toast.makeText(Register.this, "Successfully Registered User", Toast.LENGTH_SHORT).show();
-                                startActivity(new Intent(Register.this, Login.class));
-                                finish();
-                            }
-                        }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(Register.this, "Registration Failed", Toast.LENGTH_SHORT).show();
-                    }
-                });
+            //Add date checker
+            //Add numeric current weight checker
+            //Add numeric target weight checker
+
+            //Check if email and password field is empty, if password is at least 6 characters
+            if (!rEmail.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(rEmail).matches()) {
+                if ((!rPassword.isEmpty()) && (rPassword.length() >= 6)) {
+                    //Add check if email exists
+
+                    //Put all the strings into a UserClass to upload
+                    UserClass User = new UserClass(fName, lName, rEmail, rPassword, rBirthday, rCWeight, rTWeight);
+
+                    //Create user in Firebase Auth and Storage
+                    firebaseAuth.createUserWithEmailAndPassword(rEmail, rPassword)
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    //Add Send User a Verification Email
+                                    String[] splitEmail = rEmail.split("@");
+                                    String username = splitEmail[0];
+                                    ref.child(username).setValue(User);
+                                    Toast.makeText(Register.this, "Successfully Registered User", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(Register.this, Login.class));
+                                    finish();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(Register.this, "Registration Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                }
+                else if (rPassword.length() < 6) {
+                    registerPassWD.setError("Must be at least 6 characters");
+                    registerPassWD.requestFocus();
+                }
+                else {
+                    registerPassWD.setError("Must enter a password");
+                    registerPassWD.requestFocus();
+                }
+            }
+            else if (rEmail.isEmpty()) {
+                registerEmail.setError("Must enter an Email Address");
+                registerEmail.requestFocus();
             }
             else {
-                registerPassWD.setError("Must enter a password");
+                registerEmail.setError("Please Enter a Correct Email Address");
+                registerEmail.requestFocus();
             }
         }
-        else if (rPassword.isEmpty()){
-            registerEmail.setError("Must enter an Email Address");
+        else if(fName.isEmpty()){
+            firstName.setError("First name required!");
+            firstName.requestFocus();
         }
-        else{
-            registerEmail.setError("Please Enter a Correct Email Address");
+        else if(lName.isEmpty()){
+            lastName.setError("Last Name required!");
+            lastName.requestFocus();
+        }
+        else if(rBirthday.isEmpty()){
+            birthday.setError("Birthday required!");
+            birthday.requestFocus();
+        }
+        else if(rCWeight.isEmpty()){
+            currentWeight.setError("Your current weight is required!");
+            currentWeight.requestFocus();
+        }
+        else if(rTWeight.isEmpty()){
+            targetWeight.setError("Your target weight is required!");
+            targetWeight.requestFocus();
         }
     }
 }
